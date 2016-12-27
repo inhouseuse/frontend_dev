84267accdc3987a361cd6a3b7d042392e9c78b36
nokogiring <beefupme@gmail.com>
2016-12-27 23:38:11 +0900

    Vue.jsのforループを追加

--- VUEJS/js/main.js
+++ VUEJS/js/main.js
@@ -1,36 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     var app = new Vue({
-        el: '#app',
+        el: "#app",
         data: {
-            message: 'Hello Vue!'
+            message: "Hello Vue!"
         }
     });
 
     var app2 = new Vue({
-        el: '#app-2',
+        el: "#app-2",
         data: {
             url: "https://jp.vuejs.org/"
         }
-    })
+    });
 
     var app3 = new Vue({
-        el: '#app-3',
+        el: "#app-3",
         data: {
             flg: true
         }
-    })
-    
-    document.getElementById("btn").addEventListener("click",function(){
-        app3.flg = false;
     });
 
-    var app6 = new Vue({
-        el: '#app-4',
+    document.getElementById("btn").addEventListener("click", function () {
+        app3.flg = app3.flg ? false : true;
+    });
+
+    var _items = [{
+        text: "Apple",
+        url: "http://www.apple.com"
+    }, {
+        text: "Amazon",
+        url: "https://www.amazon.co.jp/"
+    }, {
+        text: "Google",
+        url: "https://www.google.co.jp/"
+    }, {
+        text: "Microsoft",
+        url: "https://www.microsoft.com/ja-jp/"
+    }];
+
+    var app4 = new Vue({
+        el: "#app-4",
         data: {
-            message: ''
+            items: _items
         }
-    })
+    });
+
+    var app5 = new Vue({
+        el: "#app-5",
+        data: {
+            message: ""
+        }
+    });
 
 
 
